3401381c5553604e5558d26374f23137928d4c5f
Yana Makogon <yana.mak89@gmail.com>
2024-10-24 18:17:34 +0200

    tasks completed

---
+++ ReflectorApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Введить шлях до .dll: ");
+        string pathToDll = Console.ReadLine();
+
+        if (File.Exists(pathToDll))
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(pathToDll);
+                Console.WriteLine("\nІнформація про складання: ");
+                Console.WriteLine("\nНазва складання: " + assembly.FullName);
+
+                Console.WriteLine("\nТипи, що входять до складу збірки: ");
+                foreach (Type type in assembly.GetTypes())
+                {
+                    Console.WriteLine("\nТип: " + type.FullName);
+                    Console.WriteLine("Базовий тип: " + type.BaseType);
+                    
+                    Console.WriteLine("Властивості: ");
+                    foreach (PropertyInfo property in type.GetProperties())
+                    {
+                        Console.WriteLine("- " + property.Name + " : " + 
+                                          property.PropertyType);
+                    }
+
+                    Console.WriteLine("Методи:");
+                    foreach (MethodInfo method in type.GetMethods())
+                    {
+                        Console.WriteLine("- " + method.Name);
+                    }
+
+                    Console.WriteLine("Поля:");
+                    foreach (FieldInfo field in type.GetFields())
+                    {
+                        Console.WriteLine("- " + field.Name + " : " + field.FieldType);
+                    }
+
+                    Console.WriteLine("Конструктори:");
+                    foreach (ConstructorInfo constructor in type.GetConstructors())
+                    {
+                        Console.WriteLine("- " + constructor.ToString());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Помилка завантаження складання: " + e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл .dll не знайдено за вказаним шляхом: " + pathToDll);
+        }
+    }
+}
---
+++ TemperatureReflection/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string pathToDll = @"/Users/yanamakogon/RiderProjects/TemperatureConvertorApp/TemperatureConvertorApp/bin/Debug/net8.0/TemperatureConvertorApp.dll";
+
+        if (File.Exists(pathToDll))
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(pathToDll);
+                
+                Console.WriteLine("Перевірка типів у збірці:");
+                foreach (Type type in assembly.GetTypes())
+                {
+                    Console.WriteLine("Знайдено тип: " + type.FullName);
+                }
+                
+                PerformConversion(assembly, "TemperatureConvertorApp.CelsiusToFahrenheitConverter", 
+                    25.0, "\u00b0C", "\u00b0F");
+                PerformConversion(assembly, "TemperatureConvertorApp.FahrenheitToCelsiusConverter", 
+                    77.0, "\u00b0F", "\u00b0C");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Помилка завантаження збірки: " + ex.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл .dll не знайдено за вказаним шляхом: " + pathToDll);
+        }
+    }
+
+    private static void PerformConversion(Assembly assembly, 
+        string converterTypeName, double inputValue,
+        string inputUnit, string outputUnit)
+    {
+        Type converterType = assembly.GetType(converterTypeName);
+        if (converterType != null)
+        {
+            try
+            {
+                object converterInstance =
+                    Activator.CreateInstance(converterType, new object[] { inputValue });
+
+                MethodInfo convertMethod = converterType.GetMethod("Convert");
+                if (convertMethod != null)
+                {
+                    object result = convertMethod.Invoke(converterInstance, null);
+                    Console.WriteLine("{0}{1} = {2}{3}", inputValue, inputUnit, result.GetType, outputUnit);
+                }
+                else
+                {
+                    Console.WriteLine("Метод Convert не знайдено для типу: " + converterTypeName);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Помилка при конвертації для типу: " + converterTypeName + converterType
+                + " - " + ex.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Клас " + converterTypeName + " не знайдено.");
+        }
+    }
+}
